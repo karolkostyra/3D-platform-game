763c4a178275fd32b3baa8584feb69bcd6798a35
koei123 <44778763+koei123@users.noreply.github.com>
2019-12-17 15:14:45 +0100

    Portal
    stworzenie portalu do nastepnego poziomu

---
+++ 3D-platform-game/Assets/Scripts/DarkPortal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DarkPortal : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] string setLevel = "level2";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                Debug.Log("Teleport do " + setLevel);
+                SceneManager.LoadScene(setLevel);
+            }
+        }
+        
+    
+}
